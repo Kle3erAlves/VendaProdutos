d310f571cabed3dedca15524e295925f826a0394
Kle3er Alves <klebergithub@gmail.com>
2022-01-09 00:06:46 -0300

    PROJETO CRIADO PARA VENDA DE PRODUTOS

---
+++ VendaProdutos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VendaProdutos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
